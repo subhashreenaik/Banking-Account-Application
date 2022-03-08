4d6acf0b0e8ecd5414a803e7690f22f6ac53d20a
Subhashree naik <subhashree.naik@gmail.com>
2022-03-08 08:23:46 +0530

    Welcome to Banking Account Application

---
+++ src/com/workshop/Banking/SavingAccount.java
@@ -0,0 +1,12 @@
+package com.workshop.Banking;
+
+public class SavingAccount {
+	
+    static final double annualInterstaRate = 4;
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Welcome to Saving Acount Application");
+	}
+
+}
